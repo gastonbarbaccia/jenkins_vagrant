10eb822e80b15218eefbdca3b6a388722d134f85
Gaston Barbaccia <gastonbarbaccia@hotmail.com>
2024-02-01 21:41:02 -0300

    first commit

---
+++ Vagrantfile
@@ -0,0 +1,20 @@
+# -*- mode: ruby -*-
+# vi: set ft-ruby :
+
+Vagrant.configure("2") do |config|
+       config.vm.box = "ubuntu/bionic64"
+       config.vm.hostname = "master"
+       config.vm.define "master"
+       config.vm.network "forwarded_port", guest: 8080, host: 8080
+       config.vm.network "public_network"   #Esto solo para linux
+  # config.vm.network "private_network", ip: "192.168.33.13"  #Esto es solo para windows
+  
+       config.vm.provider "virtualbox" do |v|
+        v.name = "master"
+        v.memory = "4096"
+        v.cpus = 4
+       end
+       config.vm.provision "shell" do |s|
+        s.path = 'jenkins.sh'
+       end
+end
---
+++ jenkins.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+
+
+echo "Installing default-java"
+sudo apt-get -y install default-jre > /dev/null 2>&1
+sudo apt-get -y install default-jdk > /dev/null 2>&1
+
+echo "Installing jenkins"
+sudo apt-get -y install jenkins > /dev/null 2>&1
+sudo service jenkins start
+
+sleep 1m
+
+echo "Installing Jenkins Plugins"
+JENKINSPWD=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
+echo $JENKINSPWD
